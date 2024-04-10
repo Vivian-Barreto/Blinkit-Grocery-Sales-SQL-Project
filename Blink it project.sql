118e06d461d3d3ffe5ff28560ac4868eebb4eb0b
Vivian-Barreto <166646273+Vivian-Barreto@users.noreply.github.com>
2024-04-11 00:53:02 +0530

    Blinkit project.sql

--- Blink it project.sql
+++ Blink it project.sql
@@ -1,238 +1,250 @@
 show databases;
-
 use vivian;
 
-#1
+#Q1 Import data from table sql_project sp using the provided CSV File.
 select * from vivian.sql_project sp ;
-#2
+
+#Q2 Write an SQL query to show all Item_Identifier.
 select Item_Identifier from sql_project sp ;
-#3
+
+#Q3 Write an SQL query to show count of total Item_Identifier.
 select count(Item_Identifier) from sql_project sp ;
-#4
+
+#Q4 Write an SQL query to show maximum Item Weight.
 select max(ITEM_WEIGHT) from sql_project sp ; 
 select  ITEM_WEIGHT from sql_project sp ;
-#5
+
+#Q5 Write an SQL query to show minimum Item Weight.
 select min(ITEM_WEIGHT)from sql_project sp ;
-#6
+
+#Q6 Write an SQL query to show average Item_Weight
 select avg(Item_Weight)from sql_project sp ;
-#7
+
+#Q7 Write an SQL query to show count of Item_Fat_Content where Item_Fat_Content is Low Fat.
 select count(Item_Fat_Content)from sql_project sp 
 where Item_Fat_Content ='Low Fat';
-#8
+
+#Q8 Write an SQL query to show count of Item_Fat_Content WHERE Item_Fat_Content is Regular.
 select count(item_fat_content)from sql_project sp 
 where Item_Fat_Content ='Regular';
 select count(Item_Fat_Content) from sql_project sp ;
 
-#9
+#Q9 Write an SQL query to show maximum Item_MRP.
 select max(item_mrp) from sql_project sp ; 
-#10
+
+#Q10 Write an SQL query to show minimum Item_MRP.
 select min(item_mrp)from sql_project sp ; 
 
-#11
+#Q11 Write an SQL query to show Item_Identifier , Item_Fat_Content ,Item_Type, Item_MRP 
+whose Item_MRP is greater than 200.
 select  Item_Identifier , Item_Fat_Content ,Item_Type ,Item_MRP 
 from sql_project sp 
 where Item_MRP >200 ;
-#12
+
+#Q12 Write an SQL query to show maximum Item_MRP WHERE Item_Fat_Content is Low Fat.
 select Item_Fat_Content , max(Item_MRP) from sql_project sp 
 where item_fat_content="low fat";
 
-#13
+#Q13 Write an SQL query to show minimum Item_MRP whose Item_Fat_Content is Low Fat.
 select Item_Fat_Content , min(Item_MRP)
 from sql_project sp 
 where item_fat_content="low fat";
 
-#14
+#Q14 Write an SQL query to show ALL DATA WHERE item MRP is BETWEEN 50 to 100.
 select * from sql_project sp 
 where Item_MRP between 50 and 100;
 
-#15
+#Q15 Write an SQL query to show ALL UNIQUE value of Item_Fat_Content.
 select distinct item_fat_content from sql_project sp;
 
-#16
+#Q16 Write an SQL query to show ALL UNIQUE value of  Item_Type.
 select distinct item_type from sql_project sp ;
 
-#17
+#Q17 Write an SQL query to show ALL DATA in descending ORDER by Item MRP.
 select * from sql_project sp order by Item_MRP desc;
 
-#18
+#Q18 Write an SQL query to show ALL DATA in ascending ORDER by Item_Outlet_Sales.
 select * from sql_project sp order by Item_Outlet_Sales asc;
 
-#19
+#Q19 Write an SQL query to show ALL DATA in ascending by Item_Type. 
 select * from sql_project sp order by Item_Type asc;
 
-#20
+#Q20 Write an SQL query to show DATA of item_type dairy & Meat.
 select * from sql_project sp where Item_Type in ('Dairy','Meat');
 
-#21 
+#Q21 Write an SQL query to show ALL UNIQUE value of Outlet_Size.
 select distinct outlet_size from sql_project sp ;
 
-#22
+#Q22 Write an SQL query to show ALL UNIQUE value of Outlet_Location_Type.
 select distinct outlet_location_type from sql_project sp ;
 
-#23
+#Q23 Write an SQL query to show ALL UNIQUE value of Outlet_Type.
 select distinct Outlet_Type from sql_project sp ;
 
-#24
+#Q24 Write an SQL query to show count of number of items by Item_Type  and order it in descending order.
 select Item_Type , count(item_identifier) No_of_item 
 from sql_project sp 
 group by item_type
 order by No_of_item desc;
 
-#25
+#Q25 Write an SQL query to show count of number of items by Outlet_Size and ordered it in ascending order.
 select outlet_size , count(item_identifier) No_of_item
 from sql_project sp 
 group by Outlet_Size 
 order by No_of_item  asc;
 
-#26
+#Q26 Write an SQL query to show count of number of items by Outlet_Type  and ordered it in descending order.
 select Outlet_Type  , count(item_identifier) No_of_item
 from sql_project sp 
 group by Outlet_Type  
 order by No_of_item  desc;
 
-#27
+#Q27 Write an SQL query to show count of items by Outlet_Location_Type and order it in descending order.
 select Outlet_Location_Type  , count(item_identifier) No_of_item
 from sql_project sp 
 group by Outlet_Location_Type  
 order by No_of_item  desc;
 
-#28
+#Q28 Write an SQL query to show maximum MRP by Item_Type.
 select item_type , max(item_mrp) Max_mrp
 from sql_project sp 
 group by Item_Type ;
 
-#29
+#Q29 Write an SQL query to show minimum MRP by Item_Type.
 select item_type , min(item_mrp) Min_mrp
 from sql_project sp 
 group by Item_Type ;
 
-#30
+#Q30 Write an SQL query to show minimum MRP by Outlet_Establishment_Year and order it in descending order.
 select outlet_establishment_year , min(item_mrp) as minimum_mrp
 from sql_project sp 
 group  by Outlet_Establishment_Year 
 order by minimum_mrp desc;
 
-#31
+#Q31 Write an SQL query to show maximum MRP by Outlet_Establishment_Year and order it in descending order.
 select outlet_establishment_year , max(item_mrp) as maximum_mrp
 from sql_project sp 
 group  by Outlet_Establishment_Year 
 order by maximum_mrp desc;
 
-#32
+#Q32 Write an SQL query to show average MRP by Outlet_Size and order it in descending order.
 select outlet_size ,avg(item_mrp) Average_mrp
 from sql_project sp 
 group by Outlet_Size 
 order by Average_mrp desc;
 
-#33
+#Q33 Write an SQL query to Average MRP by Outlet_Type and ordered in ascending order.
 select outlet_type ,avg(item_mrp) Average_mrp
 from sql_project sp 
 group by Outlet_type 
 order by Average_mrp asc;
 
-#34
+#Q34 Write an SQL query to show maximum MRP by Outlet_Type.
 select Outlet_Type ,max(item_mrp)as Maximum_mrp
 from sql_project sp 
 group by Outlet_Type 
 order by maximum_mrp;
 
-#35
+#Q35 Write an SQL query to show maximum Item_Weight by Item_Type.
 select Item_Type ,max(Item_Weight) Maximum_weight
 from sql_project sp 
 group by item_type
 order by maximum_weight;
 
-#36
+#Q36 Write an SQL query to show maximum Item_Weight by Outlet_Establishment_Year.
 select outlet_establishment_year , max(item_weight) Max_weight
 from sql_project sp 
 group by Outlet_Establishment_Year 
 order by Max_weight;
 
-#37
+#Q37 Write an SQL query to show minimum Item_Weight by Outlet_Type.
 select outlet_type ,min(item_weight) Min_weight
 from sql_project sp 
 group by Outlet_Type 
 order by Min_weight;
 
-#38
+#Q38 Write an SQL query to show average Item_Weight by Outlet_Location_Type and arrange it by descending order.
 select  outlet_establishment_year , avg(item_weight) as Avg_weight 
 from sql_project sp 
 group by Outlet_Establishment_Year 
 order by Avg_weight desc;
 
-#39
+#Q39 Write an SQL query to show maximum Item_Outlet_Sales by Item_Type.
 select item_type , max(item_outlet_sales) as Max_outlet_sales
 from sql_project sp 
 group by Item_Type 
 order by Max_outlet_sales ;
 
-#40
+#Q40 Write an SQL query to show minimum Item_Outlet_Sales by Item_Type.
 select item_type , min(item_outlet_sales) as Min_outlet_sales
 from sql_project sp 
 group by Item_Type 
 order by Min_outlet_sales ;
 
-#41
+#Q41 Write an SQL query to show minimum Item_Outlet_Sales by Outlet_Establishment_Year.
 select Outlet_Establishment_Year  , min(item_outlet_sales) as Min_outlet_sales
 from sql_project sp 
 group by Outlet_Establishment_Year  
 order by Min_outlet_sales ;
 
-#42
+#Q42 Write an SQL query to show maximum Item_Outlet_Sales by
+Outlet_Establishment_Year and order it by descending order.
 select item_type , max(item_outlet_sales) as Max_outlet_sales
 from sql_project sp 
 group by Item_Type 
 order by Max_outlet_sales desc;
 
-#43
+#Q43 Write an SQL query to show average Item_Outlet_Sales by Outlet_Size and order it it descending order.
 select outlet_size , avg(item_outlet_sales) as Avg_outlet_sales
 from sql_project sp 
 group by Outlet_Size 
 order by Avg_outlet_sales desc;
 
-#44
+#Q44 Write an SQL query to show average Item_Outlet_Sales by Outlet_Type.
 select Outlet_Type , avg(item_outlet_sales) as Avg_outlet_sales
 from sql_project sp 
 group by Outlet_Type  
 order by Avg_outlet_sales;
 
-#45
+#Q45 Write an SQL query to show maximum Item_Outlet_Sales by Outlet_Type.
 select outlet_type , max(item_outlet_sales) as Max_outlet_sales
 from sql_project sp 
 group by Outlet_Type
 order by Max_outlet_sales ;
 
-#46
+#Q46 Write an SQL query to show total Item_Outlet_Sales by Item_Type.
 select item_type ,sum(item_outlet_sales) Sum_outlet_sales
 from sql_project sp 
 group by Item_Type 
 order by Sum_outlet_sales ;
 
-#47
+#Q47 Write an SQL query to show total Item_Outlet_Sales by Item_Fat_Content.
 select Item_Fat_Content  , sum(ITEM_OUTLET_SALES) Total_outletsales
 from sql_project sp 
 group by Item_Fat_Content  
 order by total_outletsales ;
 
-#48
+#Q48 Write an SQL query to show maximum Item_Visibility by Item_Type.
 select item_type , max(item_visibility) Max_visibility
 from sql_project sp 
 group by Item_Type 
 order  by Max_visibility ;
 
-#49
+#Q49 Write an SQL query to show Minimum Item_Visibility by Item_Type. 
 select item_type , min(item_visibility) Min_visibility
 from sql_project sp 
 group by Item_Type 
 order  by Min_visibility ;
 
-#50
+#Q50 Write an SQL query to show total Item_Outlet_Sales by Item_Type 
+but only WHERE Outlet_Location_Type is Tier 1.
 select item_type ,sum(item_outlet_sales) as Total_outlet_sales ,Outlet_Location_Type 
 from sql_project sp where Outlet_Location_Type =('tier 1')
 group by Item_Type 
 order by Total_outlet_sales ;
 
-#51
+#Q51 Write an SQL query to show total Item_Outlet_Sales by Item_Type 
+WHERE Item_Fat_Content is ONLY Low Fat & LF.
 select item_type , sum(item_outlet_sales)as Total_outlet_sales 
 from  sql_project sp 
 where Item_Fat_Content in('Low Fat','LF')
